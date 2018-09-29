ba67d69f3f535fd64729b148333182db14b38345
Ahmed <Ahmed.Dajani@gmail.com>
2018-09-29 11:21:49 -0400

    initial version

---
+++ Logistics/Controllers/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using Logistics.Exceptions;
+using Logistics.Helpers;
+using System.Web.Mvc;
+
+namespace Logistics.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //base.OnActionExecuted(filterContext);
+            //if (filterContext.ActionDescriptor.ActionName != "NotAuthorized" &&
+            //    filterContext.ActionDescriptor.ActionName != "Error")
+            //{
+            //    AuthenticateUser(filterContext.ActionDescriptor.ControllerDescriptor.ControllerName, filterContext.ActionDescriptor.ActionName);
+            //}
+            try
+            {
+                AuthenticateUser();
+            }
+            catch (SessionExpiredException exc)
+            {
+                filterContext.Result = RedirectToAction("Index", "Login");
+            }
+        }
+
+        protected void AuthenticateUser()
+        {
+            if (SessionHelper.SatisUser == null)
+                throw new SessionExpiredException();
+        }
+    }
+}
---
+++ Logistics/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Logistics.Helpers;
+using System.Web.Mvc;
+
+namespace Logistics.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public HomeController()
+        {            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //protected override void OnException(ExceptionContext filterContext)
+        //{
+        //    Exception exception = filterContext.Exception;
+        //    //Logging the Exception
+        //    filterContext.ExceptionHandled = true;
+
+
+        //    var Result = this.View("Error", new HandleErrorInfo(exception,
+        //        filterContext.RouteData.Values["controller"].ToString(),
+        //        filterContext.RouteData.Values["action"].ToString()));
+
+        //    filterContext.Result = Result;
+
+        //}
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
