19d775427a79bf5d0c2b541fa45a7ed1bc3f3693
Ahmed <Ahmed.Dajani@gmail.com>
2018-12-08 20:48:19 -0500

    added code to startup to create admin user if not found. updated login to username rather than email, and refactored dependant code accrdingly. removed the websetup project. excluded the content folder (we don't need it)

--- Logistics/Models/AccountViewModels.cs
+++ Logistics/Models/AccountViewModels.cs
@@ -48,18 +48,22 @@ namespace Logistics.Models
 
     public class LoginViewModel
     {
+        //[Required]
+        //[Display(Name = "Email")]
+        //[EmailAddress]
+        //public string Email { get; set; }
+
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
+        //[Display(Name = "Remember me?")]
+        //public bool RememberMe { get; set; }
     }
 
     public class RegisterViewModel
--- Logistics/Startup.cs
+++ Logistics/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
+using System;
 
 [assembly: OwinStartupAttribute(typeof(Logistics.Startup))]
 namespace Logistics
@@ -13,66 +14,98 @@ namespace Logistics
         {
             ConfigureAuth(app);
             CreateRolesandUsers();
+            CreateAdmin();
             //AddToAdmin();
         }
 
         // In this method we will create default User roles and Admin user for login   
         private void CreateRolesandUsers()
         {
-            ApplicationDbContext context = new ApplicationDbContext();
-
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-
-
-            // Create Roles witihin Identity    
-            if (!roleManager.RoleExists("Admin"))
+            using (ApplicationDbContext context = new ApplicationDbContext())
             {
-                // Create Admin role   
-                var role = new IdentityRole();
-                role.Name = "Admin";
-                roleManager.Create(role);
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
-            }
+                // Create Roles witihin Identity    
+                if (!roleManager.RoleExists("Admin"))
+                {
+                    // Create Admin role   
+                    var role = new IdentityRole { Name = "Admin" };
+                    roleManager.Create(role);
+                }
 
-            // Creating Manager role    
-            if (!roleManager.RoleExists("Manager"))
+                // Creating Manager role    
+                if (!roleManager.RoleExists("Manager"))
+                {
+                    var role = new IdentityRole { Name = "Manager" };
+                    roleManager.Create(role);
+                }
+
+                // Creating Employee role    
+                if (!roleManager.RoleExists("Employee"))
+                {
+                    var role = new IdentityRole { Name = "Employee" };
+                    roleManager.Create(role);
+                }
+            }                
+        }
+
+        /// <summary>
+        /// Create an Admin user if doesnt exist.
+        /// </summary>
+        private void CreateAdmin()
+        {
+            var user = new ApplicationUser
             {
-                var role = new IdentityRole();
-                role.Name = "Manager";
-                roleManager.Create(role);
+                UserName = "Admin",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                TwoFactorEnabled = false,
+                LockoutEnabled = false,
+                AccessFailedCount = 0
+            };
 
-            }
-
-            // Creating Employee role    
-            if (!roleManager.RoleExists("Employee"))
+            using (var cxt = new ApplicationDbContext())
             {
-                var role = new IdentityRole();
-                role.Name = "Employee";
-                roleManager.Create(role);
+                try
+                {
+                    var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(cxt));
+                    var admin = userManager.FindByName(user.UserName);
+                    if (admin == null)
+                    {
+                        var result = userManager.Create(user, "satisapp");
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRole(user.Id, "Admin");
+                            cxt.SaveChanges();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
             }
         }
 
-        //private void AddToAdmin()
-        //{
-        //    ApplicationDbContext context = new ApplicationDbContext();
+        private void AddToAdmin()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
 
-        //    var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-        //    string userName = "rickywhite@outlook.com";
-        //    string roleName = "Admin";
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            string userName = "Ahmed";
+            string roleName = "Admin";
 
-        //    try
-        //    {
-        //        var user = UserManager.FindByName(userName);
-        //        UserManager.AddToRole(user.Id, roleName);
-        //        context.SaveChanges();
-        //    }
-        //    catch
-        //    {
-        //        throw;
-        //    }
-        //}
-
+            try
+            {
+                var user = UserManager.FindByName(userName);
+                UserManager.AddToRole(user.Id, roleName);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 
     
