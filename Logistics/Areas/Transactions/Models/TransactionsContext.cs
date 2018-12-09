45d5688d26770f0cc5bca629dd41e873b0caf9f7
Ricky White <rickywhite@outlook.com>
2018-12-09 11:32:35 -0500

    Set up folder structure for module

---
+++ Logistics/Areas/Transactions/Models/TransactionsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Logistics.Areas.Transactions.Models
+{
+    public class TransactionsContext : DbContext
+    {
+        public TransactionsContext() : base("name=SatisDB")
+        {
+        }
+
+    }
+}
