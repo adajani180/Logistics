72c45706fad911d923456d7d4724820a6c5c4aaf
Ricky White <rickywhite@outlook.com>
2018-12-10 14:16:17 -0500

    Added the main ManageConroller, View and sidebar links. Base Model added, but need more work.

--- Logistics/Areas/Transactions/Models/TransactionsContext.cs
+++ Logistics/Areas/Transactions/Models/TransactionsContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,24 @@ namespace Logistics.Areas.Transactions.Models
         public TransactionsContext() : base("name=SatisDB")
         {
         }
+
+        public int Id { get; set; }
+
+        //[ForeignKey()]
+        public int ItemNum { get; set; }
+
+        //[ForeignKey()]
+        public int EquipNum { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public string EmployeeId { get; set; }
+
+        //[ForeignKey()]
+        public int NewEmployeeId { get; set; }
+        public string Vin { get; set; }
+        public string UnitId { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
 
     }
 }
